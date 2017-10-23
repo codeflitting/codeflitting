be42d951ba2e630b88db0ca9454389ef511df2fb
chenpengyu <chenpengyu1@jd.com>
2017-10-23 18:52:28 +0800

    add header style

--- codeflitting/urls.py
+++ codeflitting/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^quote/', include('codeflitting.quote.urls')),
     url(r'^', include('codeflitting.core.urls')),
 ]

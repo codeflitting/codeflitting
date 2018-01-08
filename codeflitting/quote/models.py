cbde69332e0b1737586db0de57a9b3e62ba8534d
PY <codeflitting@gmail.com>
2018-01-08 23:19:25 +0800

    up to date

--- codeflitting/quote/models.py
+++ codeflitting/quote/models.py
@@ -7,7 +7,7 @@ class Wisdom(BaseModel):
     chinese = models.TextField('格言-中文')
     views = models.PositiveIntegerField('浏览量', default=0)
     likes = models.PositiveIntegerField('点赞数', default=0)
-    author = models.ForeignKey('Author', verbose_name='作者', blank=True)
+    author = models.ForeignKey('Author', verbose_name='作者', blank=True, on_delete=models.CASCADE)
     tags = models.ManyToManyField('Tag', verbose_name='标签', blank=True)
 
 

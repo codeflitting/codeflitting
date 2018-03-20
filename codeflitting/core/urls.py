67fcaef175ce05f46f4b399befd30fa156c6f7a2
1M <yizao.cpy@alibaba-inc.com>
2018-03-20 13:58:30 +0800

    add test.html

--- codeflitting/core/urls.py
+++ codeflitting/core/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='core/index.html'), name="core-index"),
+    url(r'^test', TemplateView.as_view(template_name='test/test.html'), name="core-test"),
 ]

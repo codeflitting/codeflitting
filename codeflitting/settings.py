e5ef5e3e932462727bde6642b6ddfa342dd633ea
1M <yizao.cpy@alibaba-inc.com>
2018-03-26 20:12:03 +0800

    up to date

--- codeflitting/settings.py
+++ codeflitting/settings.py
@@ -55,10 +55,10 @@ ROOT_URLCONF = 'codeflitting.urls'
 
 TEMPLATES = [
     {
-        'BACKEND' : 'django.template.backends.django.DjangoTemplates',
-        'DIRS'    : [],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS' : {
+        'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'codeflitting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codeflitting',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
